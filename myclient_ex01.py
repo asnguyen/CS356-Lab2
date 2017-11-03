f9ddd32b7ae1c18316b087036c75c1fc644ca357
asnguyen <asnguyen@users.noreply.github.com>
2017-11-03 12:29:08 -0500

    final comments to myclient_ex01

--- myclient_ex01.py
+++ myclient_ex01.py
@@ -67,27 +67,37 @@ def main():
  print "End"
 
 #HELPER FUNCTIONS
+#   split_num() takes in an integer and returns a tuple 
+#   of the higher 16 bits and the lower 16 bits
 def split_num(num):
  higherB = num >> 16
  lowerB = num - (higherB << 16)
  return hex(higherB), hex(lowerB)
 
+#   ones_comp_add() takes in two number and performs
+#   ones compliment addtion on the two numbers
 def ones_comp_add(num1, num2):
  mod = 1<<16
  result = num1+num2
  return result if result < mod else (result+1) % mod
 
+#   create_checksum() takes in an array of eight 16 bit numbers and 
+#   creates a checksum by adding all the numbers using one's complement
 def create_checksum(ar):
  checksum = 0
  for i in range(0,len(ar)):
   checksum = ones_comp_add(checksum,ar[i])
  return checksum
 
+#   create_cookie() create a random integer to be used the cookie and
+#   returns a tuple of the highter bits and lower bits
 def create_cookie():
  num = randrange(0,9000)
  return split_num(num)
   
 
+#   pack_packet() takes in an array of 16 bit integers and addes them 
+#   one by one to the packe 
 def pack_packet(ar):
  pkt = ''
  for i in range(0,len(ar)):
